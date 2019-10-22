fe00301f74d6f835b30840390e0e28969ebb2799
Greg Scontras <gscontras@gmail.com>
2019-10-22 16:17:19 -0700

    greek results

--- experiments/1-order-preference-Greek/results/analysis.R
+++ experiments/1-order-preference-Greek/results/analysis.R
@@ -2,36 +2,56 @@ library(ggplot2)
 library(lme4)
 library(hydroGOF)
 library(dplyr)
-source("../results/helpers.R")
 
-setwd("~/git/arabic_adjectives/experiments/2-order-preference-expanded/Submiterator-master")
+setwd("~/git/greek_adjectives/experiments/1-order-preference-Greek/results/")
+source("helpers.R")
 
-num_round_dirs = 15
-df1 = do.call(rbind, lapply(1:num_round_dirs, function(i) {
-  return (read.csv(paste(
-    'round', i, '/arabic-order-expanded.csv', sep=''),stringsAsFactors=FALSE) %>% 
-      mutate(workerid = (workerid + (i-1)*9)))}))
-df1$workerid = paste("vi.",df1$workerid)
 
-d1 = subset(df1, select=c("workerid","noun","gender","nounclass","slide_number", "predicate1", "predicate2", "class1","class2","response","language","comments","asses","gender.1","test1","test2","test3","dialect","lived","describe","years","proficiency"))
+df = read.csv("results.csv",header=T)
 
-d <- d1
+d = subset(df, select=c("age",
+                        "asses",
+                        "comments",
+                        #"describe",
+                        #"dialect", 
+                        #"education", 
+                        "enjoyment", 
+                        "gender",
+                        "language",
+                        #"lived",
+                        #"proficiency",
+                        #"test1",
+                        #"test2",
+                        #"test3",
+                        #"years",
+                        "time_in_minutes",
+                        "class1",
+                        "class2",
+                        "noun",
+                        "nounclass",
+                        "predicate1",
+                        "predicate2",
+                        "response",
+                        "slide_number",
+                        "participant_id",
+                        "time_in_minutes"))
 
-# got all the test questions correct
-d = d[d$test1=="correct"&d$test2=="correct"&d$test3=="correct",]
-# lived more than 5 years both before and after age 8 in arabic country
-d = d[d$lived=="both"&d$years=="5+",]
-# describe as arabic-arabic
-d = d[d$describe=="arabic-arabic",]
+#d <- df
 
 unique(d$language)
+d = d[d$language!="greg here"&
+        d$language!="more greg"&
+        d$language!="123"&
+        d$language!="GREG"&
+        d$language!="",
+        ]
 
-d = d[d$language != "البلوشية، العربية، الانجليزيه"&d$language!="",]
+#d = d[d$subject_information_language != "English"&d$subject_information_language!="English "&d$subject_information_language!=""&d$subject_information_language!="انجليزي",]
 #d = d[d$asses=="Yes",]
 
-length(unique(d$workerid)) #n=24
+d$workerID = d$participant_id
 
-table(d$dialect)
+length(unique(d$workerID)) #n=46 (48)
 
 t <- d
 
@@ -46,18 +66,18 @@ o$rightpredicate1 = o$predicate2
 o$rightpredicate2 = o$predicate1
 o$rightresponse = 1-o$response
 agr = o %>% 
-  select(predicate1,rightpredicate1,response,rightresponse,workerid,noun,nounclass,class1,class2) %>%
-  gather(predicateposition,predicate,predicate1:rightpredicate1,-workerid,-noun,-nounclass,-class1,-class2)
+  select(predicate1,rightpredicate1,response,rightresponse,workerID,noun,nounclass,class1,class2) %>%
+  gather(predicateposition,predicate,predicate1:rightpredicate1,-workerID,-noun,-nounclass,-class1,-class2)
 agr$correctresponse = agr$response
 agr[agr$predicateposition == "rightpredicate1",]$correctresponse = agr[agr$predicateposition == "rightpredicate1",]$rightresponse
 agr$correctclass = agr$class1
 agr[agr$predicateposition == "rightpredicate1",]$correctclass = agr[agr$predicateposition == "rightpredicate1",]$class2
 head(agr[agr$predicateposition == "rightpredicate1",])
-agr$response = NULL
+agr$trails_response = NULL
 agr$rightresponse = NULL
 agr$class1 = NULL
 agr$class2 = NULL
-nrow(agr) #XXX
+nrow(agr) #2392
 #write.csv(agr,"~/git/arabic_adjectives/experiments/2-order-preference-expanded/results/arabic-naturalness-duplicated.csv")
 agr$correctresponse = 1 - agr$correctresponse
 
